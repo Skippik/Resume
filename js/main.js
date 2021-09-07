265fa02c22518fcc0dc523815b3024510e9761da
Evgenii <Skippik1111@gmail.com>
2021-09-07 14:08:11 +0300

    init: CV

--- js/main.js
+++ js/main.js
@@ -1,11 +1,46 @@
-const buttonEn = document.getElementById('En');
-const buttonRu = document.getElementById('Ru');
 
 
-buttonEn.addEventListener('click', function() {
-    document.location.href = "https://skippik.github.io/Resume/language/En.html";
-})
+function scrollTo(to, duration = 700) {
+    const
+        element = document.scrollingElement || document.documentElement,
+        start = element.scrollTop,
+        change = to - start,
+        startDate = +new Date(),
+        easeInOutQuad = function (t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        },
+        animateScroll = function () {
+            const currentDate = +new Date();
+            const currentTime = currentDate - startDate;
+            element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
+            if (currentTime < duration) {
+                requestAnimationFrame(animateScroll);
+            }
+            else {
+                element.scrollTop = to;
+            }
+        };
+    animateScroll();
+}
 
-buttonRu.addEventListener('click', function() {
-    document.location.href = "https://skippik.github.io/Resume/language/Ru.html";
-})
+document.addEventListener('DOMContentLoaded', function () {
+    let btn = document.querySelector('#toTop');
+    window.addEventListener('scroll', function () {
+        // Если прокрутили дальше 599px, показываем кнопку
+        if (pageYOffset > 100) {
+            btn.classList.add('show');
+            // Иначе прячем
+        } else {
+            btn.classList.remove('show');
+        }
+    });
+
+    // При клике прокручиываем на самый верх
+    btn.onclick = function (click) {
+        click.preventDefault();
+        scrollTo(0, 400);
+    }
+});
