f95f87386a97727a4266524e495226e0812a8647
Evgenii <Skippik1111@gmail.com>
2021-09-07 22:23:17 +0300

    feat: adaptive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 
 
+
+// Плавный скрол====================================
 function scrollTo(to, duration = 700) {
     const
         element = document.scrollingElement || document.documentElement,
@@ -43,4 +45,67 @@ document.addEventListener('DOMContentLoaded', function () {
         click.preventDefault();
         scrollTo(0, 400);
     }
+});
+
+// получаем необходимые элементы!==========================================
+const body = document.getElementById('body')
+const burgerMenuButton = document.getElementById('menu-icon');
+const menuLinks = document.querySelectorAll('.nav__link[data-goto]');
+const burgerMenu = document.getElementById('menu-burger');
+
+
+
+// Плавная прокрутка по якорям!========================================
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick)
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            // if(burgerMenu.classList.contains('active')) {
+            //     burgerMenuButton.classList.remove('.active');
+            //     body.classList.remove('.no_scroll');
+            //     burgerMenu.classList.remove('.active');
+            // }
+
+            window.scroll({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            });
+            e.preventDefault();
+
+    }
+}
+
+// Бургер меню ===================================================
+if(burgerMenuButton) {
+    burgerMenuButton.addEventListener('click', function(e) {
+        burgerMenuButton.classList.toggle('active');
+        body.classList.toggle('no_scroll');
+        burgerMenu.classList.toggle('active');
+    })
+}
+
+//navbar links
+
+const navbar = document.querySelector("#menu-burger");
+a = navbar.querySelectorAll("#burgerMenu");
+console.log(a);
+
+
+a.forEach(function (element) {
+    element.addEventListener("click", function () {
+        for (let i = 0; i < a.length; i++) {
+
+            a[i].classList.remove("active")
+        }
+        this.classList.add("active");
+        burgerMenuButton.classList.remove('active');
+        body.classList.remove('no_scroll');
+        burgerMenu.classList.remove('active');
+    })
 });
