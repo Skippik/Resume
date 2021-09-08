08db6e17336c27311383b19e39aa67d4262cb42a
Evgenii <Skippik1111@gmail.com>
2021-09-08 14:50:54 +0300

    fix: adaptive

--- js/app.js
+++ js/app.js
@@ -19,29 +19,22 @@ const menuLinks = document.querySelectorAll('.nav__link[data-goto]');
 const burgerMenu = document.getElementById('menu-burger');
 
 // Бургер меню ===================================================
-if(burgerMenuButton) {
-  burgerMenuButton.addEventListener('click', function(e) {
-      burgerMenuButton.classList.toggle('active');
-      body.classList.toggle('no_scroll');
-      burgerMenu.classList.toggle('active');
-  })
-}
 
-//navbar links
-
-const navbar = document.querySelector("#menu-burger");
-a = navbar.querySelectorAll("#burgerMenu");
-
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a")
 
 a.forEach(function (element) {
   element.addEventListener("click", function () {
-      for (let i = 0; i < a.length; i++) {
-
-          a[i].classList.remove("active")
-      }
-      this.classList.add("active");
-      burgerMenuButton.classList.remove('active');
-      body.classList.remove('no_scroll');
-      burgerMenu.classList.remove('active');
+    for (let i = 0; i < a.length; i++) {
+      a[i].classList.remove("active")
+    }
+    this.classList.add("active");
+    document.querySelector(".navbar").classList.toggle("show");
   })
-});
+})
+
+const hamBurger = document.querySelector(".menu__icon");
+
+hamBurger.addEventListener("click", function () {
+  document.querySelector(".navbar").classList.toggle("show");
+})
